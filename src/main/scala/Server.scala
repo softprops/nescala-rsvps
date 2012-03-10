ff399af8a9c0442844e2da9710318a44a17dc04a
softprops <d.tangren@gmail.com>
2012-03-10 17:13:38 -0500

    rm templates from server

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -2,25 +2,7 @@ package nescala
 
 import unfiltered.netty.cycle.Planify
 
-import unfiltered.response.Html
-
 import java.net.URL
-
-object Templates {
-  import scala.xml._
-  def layout(body: NodeSeq) =
-    Html(
-      <html>
-        <head>
-          <link type="text/css" href="/css/app.css"/>
-          <script type="text/javascript" src="/js/app.js"></script>
-        </head>
-        <body>yep. it works</body>
-     </html>)
-
-  def index = layout(<div>it works</div>)
-
-}
 
 /** embedded server */
 object Server {
