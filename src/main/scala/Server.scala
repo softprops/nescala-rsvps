de2ec7842bc3f61f22e6c515a7d75ae6e68b73fc
softprops <d.tangren@gmail.com>
2012-03-10 16:53:06 -0500

    resources

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,19 +1,42 @@
 package com.example
 
+import unfiltered.netty.cycle.Planify
+
+import unfiltered.response.Html
+
+import java.net.URL
+
+object Templates {
+  import scala.xml._
+  def layout(body: NodeSeq) =
+    Html(
+      <html>
+        <head>
+          <link href="/css/app.css"/>
+          <script src="/js/app.js"></script>
+        </head>
+        <body>yep. it works</body>
+     </html>)
+
+  def index = layout(<div>it works</div>)
+
+}
+
+
 /** embedded server */
 object Server {
   val logger = org.clapper.avsl.Logger(Server.getClass)
-
   def main(args: Array[String]) {
     unfiltered.netty.Http(8080)
-      .handler(Palindrome)
-      .handler(Time)
-      .run { s =>
+      .resources(getClass.getResource("/www"))
+      .handler(Planify{
+        case _ => Templates.index
+      }).run { s =>
         logger.info("starting unfiltered app at localhost on port %s"
                     .format(s.port))
         unfiltered.util.Browser.open(
-          "http://127.0.0.1:%d/time".format(s.port))
+          "http://127.0.0.1:%d/".format(s.port)
+        )
       }
-    dispatch.Http.shutdown()
   }
 }
