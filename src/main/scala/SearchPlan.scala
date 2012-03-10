35b48d9b0ba05b9be62009e35783d9893066d34c
Nathan Hamblen <nathan@technically.us>
2012-03-10 17:16:09 -0500

    code to request from api

--- src/main/scala/SearchPlan.scala
+++ src/main/scala/SearchPlan.scala
@@ -6,14 +6,22 @@ import unfiltered.response._
 import unfiltered.netty._
 
 /** unfiltered plan */
-object SearchPlan extends cycle.Plan
-  with cycle.SynchronousExecution with ServerErrorResponse {
+object SearchPlan extends async.Plan
+  with ServerErrorResponse {
 
   val logger = org.clapper.avsl.Logger(getClass)
   
   def intent = {
-    case GET(Path("/search")) => 
+    case req@GET(Path("/search")) => 
       logger.debug("GET /")
-      ResponseString("Hi")
+      req.respond(ResponseString("Hi"))
+
+    case req@GET(Path("/rsvps")) =>
+      import dispatch._
+
+      for {
+        json <- Http(url("https://api.meetup.com/2/rsvps?key=5b3f7f62b575010a586d255c55322e&sign=true&event_id=44042982&page=20") > As.string)
+      } req.respond(ResponseString(json))
+
   }
 }
